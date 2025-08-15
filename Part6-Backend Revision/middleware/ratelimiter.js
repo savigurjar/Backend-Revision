b7d89616b3dd13939e653bf1a008fb7dea3caf8d
savigurjar <savigurjar1201@gmail.com>
2025-08-15 16:22:44 +0530

    sliding window

--- Part6-Backend Revision/middleware/ratelimiter.js
+++ Part6-Backend Revision/middleware/ratelimiter.js
@@ -1,19 +1,42 @@
 const redisClient = require("../config/redis");
+const crypto = require("crypto");
+
+const windowSize = 3600;
+const maxRequest = 60;
 
 const ratelimiter = async (req, res, next) => {
-  const ip = req.ip;
+  try {
+    const key = `IP${req.ip}`; //::1p me ip  ko store kr rha tha
 
-  if (!ip) throw new Error("Ip aadress not found");
+    const currentTime = Date.now() / 1000;
+    console.log("cuurent time", currentTime);
+    const windowTime = currentTime - windowSize;
+    console.log("window time", windowTime);
 
-  const count = await redisClient.incr(ip);
+    // Remove old entries
+    await redisClient.zRemRangeByScore(key, 0, windowTime);
 
-  if (count > 60) res.status(404).send("Too many requests , please try again");
+    const numOfRequest = await redisClient.zCard(key);
+    if (numOfRequest >= maxRequest)
+      throw new Error("Number of requests exceeded");
 
-  if (count == 1) await redisClient.expire(3600);
+    // Add request with secure unique value
+    const secureRandom = crypto.randomBytes(8).toString("hex"); // 16-char secure random
+    await redisClient.zAdd(key, [
+      { score: currentTime, value: `${currentTime}:${secureRandom}` },
+    ]);
 
-  console.log(count);
+    console.log(secureRandom);
 
-  next();
+    // Ensure key expires
+    await redisClient.expire(key, windowSize);
+
+    console.log(numOfRequest);
+
+    next();
+  } catch (err) {
+    res.status(401).send("error " + err);
+  }
 };
 
 module.exports = ratelimiter;
